3c3aa4048fd208c65df32fdafff22f020104975e
monkeyKing <guillaumedjio@gmail.com>
2022-01-22 23:03:26 +0100

    responsive menu 1

--- script.js
+++ script.js
@@ -15,4 +15,26 @@ function smoothScroll (e) {
     document.querySelector(href).scrollIntoView({
         behavior: "smooth"
     })
+    hideMenu()
+}
+
+// Responsive mobile menu
+const menu = document.querySelector(".nav-list")
+const hamburger = document.querySelector(".hamburger")
+const close = document.querySelector(".close")
+
+hamburger.addEventListener("click", showMenu)
+close.addEventListener("click", hideMenu)
+
+function showMenu (e) {
+    e.preventDefault()
+    hamburger.style.display = "none"
+    close.style.transform = "translateY(0)"
+    menu.style.transform = "translateY(0)"
+}
+
+function hideMenu (e) {
+    close.style.transform = "translateY(-20rem)"
+    hamburger.style.display = "block"
+    menu.style.transform = "translateY(-200%)"
 }
